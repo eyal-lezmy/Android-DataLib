17724c4113b31f08e8dc6d9c2c9b4a16c6c91bce
Eyal LEZMY <eyal@lezmy.fr>
2013-01-07 00:10:52 +0100

    Correcting the logging on this class

--- Android-DataLib-Library/src/fr/eyal/lib/data/processor/Processor.java
+++ Android-DataLib-Library/src/fr/eyal/lib/data/processor/Processor.java
@@ -164,7 +164,7 @@ public class Processor {
                 break;
         }
         HttpResponse httpResponse = httpClient.execute(httpRequest);
-        System.out.println(httpResponse.getBodyAsString());
+        Out.d(TAG, httpResponse.getBodyAsString());
         return httpResponse;
 
         //        return NetworkConnection.retrieveFromService(request.url, mHeaders, context);
