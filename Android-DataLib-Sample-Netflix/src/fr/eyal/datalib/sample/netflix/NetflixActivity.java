06008bbaeb5fc79ec9ebbf531f1ae9d4e84293a6
Eyal LEZMY <eyal@lezmy.fr>
2013-01-09 01:58:18 +0100

    Modifying the activity source code to launch actions thanks to the UI

--- Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/NetflixActivity.java
+++ Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/NetflixActivity.java
@@ -6,16 +6,89 @@ import fr.eyal.datalib.sample.netflix.data.service.NetflixDataLibActivity;
 import fr.eyal.lib.data.model.ResponseBusinessObjectDAO;
 import fr.eyal.lib.data.service.model.BusinessResponse;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NetflixActivity extends NetflixDataLibActivity {
 
+	Button mBtnSearchTitle;
+	Button mBtnGetPeople;
+	Button mBtnGetFilmography;
+	Button mBtnGetSynopsis;
+	Button mBtnGetMovie;
+	Button mBtnGetDirectors;
+	Button mBtnGetCast;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_netflix);
+		
+		mBtnGetCast = (Button) findViewById(R.id.getCast);
+		mBtnGetDirectors = (Button) findViewById(R.id.getDirectors);
+		mBtnGetFilmography = (Button) findViewById(R.id.getFilmography);
+		mBtnGetMovie = (Button) findViewById(R.id.getMovie);
+		mBtnGetPeople = (Button) findViewById(R.id.getPeople);
+		mBtnGetSynopsis = (Button) findViewById(R.id.getSynopsis);
+		mBtnSearchTitle = (Button) findViewById(R.id.searchTitle);
+		
+		final int movieId = 70206672;
+		final int peopleId = 30144804;
+		
+		mBtnGetCast.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getCast(movieId, NetflixActivity.this, mRequestIds);
+			}
+		});
+
+		mBtnGetDirectors.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getDirectors(movieId, NetflixActivity.this, mRequestIds);
+			}
+		});
+		
+		mBtnGetFilmography.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getFilmography(peopleId, NetflixActivity.this, mRequestIds);
+			}
+		});
+
+		mBtnGetMovie.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getMovie(movieId, NetflixActivity.this, mRequestIds);
+			}
+		});
+
+		mBtnGetPeople.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getPeople(peopleId, NetflixActivity.this, mRequestIds);
+			}
+		});
+
+		mBtnGetSynopsis.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.getSynopsis(movieId, NetflixActivity.this, mRequestIds);
+			}
+		});
+
+		
+		mBtnSearchTitle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDataManager.searchCatalogTitle("chris", NetflixActivity.this, mRequestIds);
+			}
+		});
+
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,9 +110,8 @@ public class NetflixActivity extends NetflixDataLibActivity {
 	}
 
 	@Override
-	public void onRequestFinished(int requestId, boolean suceed,
-			BusinessResponse response) {
-		// TODO Auto-generated method stub
+	public void onRequestFinished(int requestId, boolean suceed, BusinessResponse response) {
+		
 		
 	}
 
