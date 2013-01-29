2aa6936fd92b01bb7cb033c90f6971846fa57aaf
Eyal LEZMY <eyal.lezmy+github@gmail.com>
2013-01-30 00:16:05 +0100

    Adding the NewRelease and Top100 BOs the image handling

--- Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/data/model/newreleases/ItemNewRelease.java
+++ Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/data/model/newreleases/ItemNewRelease.java
@@ -1,12 +1,32 @@
 // Start of user code fr.eyal.datalib.sample.netflix.data.model.newreleases.ItemNewRelease. DO NOT MODIFY THE GENERATED COMMENTS
 package fr.eyal.datalib.sample.netflix.data.model.newreleases;
 
+import java.lang.ref.SoftReference;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Parcel;
 
 public class ItemNewRelease extends ItemNewReleaseBase {
 
     private static final String TAG = ItemNewRelease.class.getSimpleName();
 
+    /**
+     * Image's reference
+     */
+    public SoftReference<Bitmap> image;
+    
+    /**
+     * Path of the stored image
+     */
+    public String imagePath;
+
+    /**
+     * Image's url of the Top100 item
+     */
+    public String imageUrl = null;
+
+    
     public ItemNewRelease() {
         super();
     }
@@ -17,6 +37,41 @@ public class ItemNewRelease extends ItemNewReleaseBase {
 
     public ItemNewRelease(final long id) {
         super(id);
+    }
+    
+    /**
+     * Get the movie's poster {@link Bitmap}
+     * 
+     * @return return a {@link Bitmap} corresponding to the movie's poster or <code>null</code> whether there is no loaded image.
+     */
+    public Bitmap getPoster(){
+    	if(image == null)
+    		return null;
+    	
+    	Bitmap result = image.get();
+    	
+    	if(result == null && imagePath != null)
+    		image = new SoftReference<Bitmap>(BitmapFactory.decodeFile(imagePath));
+    		
+    	return result;
+    }
+
+    /**
+     * Get the HD image's url of the item
+     * 
+     * @return the URL needed
+     */
+    public String getImageUrl(){
+    	
+    	if(imageUrl == null){
+    		String[] content = description.split("<img src=\"");
+    		content = content[1].split("\"");
+    		
+    		String result = content[0];
+    		result = result.replace("/small/", "/ghd/");
+    		imageUrl = result;
+    	}
+    	return imageUrl;
     }
 
 
--- Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/data/model/top100/ItemTop100.java
+++ Android-DataLib-Sample-Netflix/src/fr/eyal/datalib/sample/netflix/data/model/top100/ItemTop100.java
@@ -1,11 +1,30 @@
 // Start of user code fr.eyal.datalib.sample.netflix.data.model.top100.ItemTop100. DO NOT MODIFY THE GENERATED COMMENTS
 package fr.eyal.datalib.sample.netflix.data.model.top100;
 
+import java.lang.ref.SoftReference;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Parcel;
+import android.util.SparseArray;
+import android.widget.GridView;
+import fr.eyal.datalib.sample.netflix.fragment.NewReleasesAdapter;
+import fr.eyal.lib.util.FileManager;
 
 public class ItemTop100 extends ItemTop100Base {
 
-    private static final String TAG = ItemTop100.class.getSimpleName();
+	private static final String TAG = ItemTop100.class.getSimpleName();
+
+    /**
+     * Image's reference
+     */
+    public SoftReference<Bitmap> image;
+    
+    /**
+     * Path of the stored image
+     */
+    public String imagePath;
 
     /**
      * Image's url of the Top100 item
@@ -23,6 +42,27 @@ public class ItemTop100 extends ItemTop100Base {
 
     public ItemTop100(final long id) {
         super(id);
+    }
+
+    /**
+     * Get the movie's poster {@link Bitmap}
+     * 
+     * @param context the context of execution. Needed to have access to the stored images.
+     * 
+     * @return return a {@link Bitmap} corresponding to the movie's poster or <code>null</code> whether there is no loaded image.
+     */
+    public Bitmap getPoster(Context context){
+    	if(image == null)
+    		return null;
+    	
+    	Bitmap result = image.get();
+    	
+    	if(result == null)
+    		image = new SoftReference<Bitmap>(BitmapFactory.decodeFile(imagePath));
+    	else if(imagePath != null && context != null)
+    		image = new SoftReference<Bitmap>(FileManager.getPictureFromInternalCache(imagePath, context));
+    	
+    	return result;
     }
 
     /**
